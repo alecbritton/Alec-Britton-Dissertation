1643809e50dd7abc2338a5d171336c2021a68056
Alec <alec@lettapp.com>
2019-11-30 16:48:17 +0000

    function for finding pull in voltage

--- StatDC.m
+++ StatDC.m
@@ -9,18 +9,24 @@ function StatDC(L,b,E,h,g0)
     syms x;
     modeFun = @(x) (sin(lambda.*x) - sinh(lambda.*x)) + alpha_n * (cos(lambda.*x) - cosh(lambda.*x));
     modeFunSq = @(x) ((sin(lambda.*x) - sinh(lambda.*x)) + alpha_n * (cos(lambda.*x) - cosh(lambda.*x))).^2;
-    km = integral(matlabFunction(diff(modeFun(x),4)),0,1);
+    kmInt = matlabFunction(diff(modeFun(x),4) * modeFun(x));
+    km = integral(kmInt,0,1);
+    disp(kmInt);
     for vDCi1 = 0.1:0.1:20
-        ke = alpha* vDCi^2 * (1-ws)^-3 * integral((modeFunSq),0,1);
-        F = alpha*(vDCi1^2-vDCi^2)*(1-ws)^-2*integral(modeFun,0,1);
+        ke = alpha* (vDCi^2) * ((1-ws)^-3) * integral((modeFunSq),0,1);
+        F = alpha*(vDCi1^2-vDCi^2)*((1-ws)^-2)*integral(modeFun,0,1);
         if(km-ke > 0)
             a = F/(km-ke);
-            ws = ws + a * modeFun(0.5);
+            ws = (ws + a * modeFun(0.5));
             plot(vDCi1,ws,'b--.');
             title('Static analysis of electrostatically actuated microbeam');
             ylabel('Beam deflection');
             xlabel('Voltage / V');
             hold on
+        end
+        if(ws > 1)
+            disp(vDCi1);
+            return;
         end
         vDCi = vDCi1;
     end
