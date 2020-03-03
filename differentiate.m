7d7beabb7d2678b77b48313eab74a39a218449eb
Alec Britton <alec.britton1@googlemail.com>
2020-03-03 12:22:36 +0000

    Update differentiate.m

--- differentiate.m
+++ differentiate.m
@@ -1,2 +1,8 @@
-function differentiate()
+function result = differentiate(inp,order)
+    syms f(x);
+    lambda = 4.370041;
+    alpha_n = (sinh(lambda) - sin(lambda))/(cos(lambda) - cosh(lambda));
+    f(x) = (sin(lambda*x) - sinh(lambda*x)) + alpha_n * (cos(lambda*x) - cosh(lambda*x));
+    diffMode = diff(f,x,order);
+    result = double(diffMode(inp));
 end
